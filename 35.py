38ec9862507430955904fc6d8528df6d81a8e34f
henry_tujia <1319670017@qq.com>
2021-07-29 16:38:04 +0800

    udpate 3 day

--- 35.py
+++ 35.py
@@ -2,6 +2,7 @@
 Solution 1 双向指针
 """
 
+
 class Solution(object):
     def searchInsert(self, nums, target):
         """
@@ -11,7 +12,7 @@ class Solution(object):
         """
         left = 0
         right = len(nums) - 1
-        while left <=right:
+        while left <= right:
             if nums[left] == target:
                 return left
             if nums[right] == target:
@@ -23,6 +24,59 @@ class Solution(object):
         return left
 
 
+"""
+Solution2 二分查找
+"""
+
+
+class Solution2(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
+        else:
+            if nums[left] < target:
+                return left + 1
+            else:
+                return left
+
+
+"""
+Solution 3 二分查找的优化版本
+"""
+
+
+class Solution3(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid+1
+            if nums[mid] > target:
+                right = mid -1
+        return left
+
+
 if __name__ == '__main__':
-    soul = Solution()
-    print(soul.searchInsert([1,3,5,6], 7))
+    soul = Solution3()
+    print(soul.searchInsert([1, 3, 5, 6], 7))
