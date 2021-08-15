921b9166b7b03eb4ba922d94634d841672fa489e
henry_tujia <1319670017@qq.com>
2021-08-15 21:35:06 +0800

    udpate 6 day

---
+++ 876.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = head
+        fast = head
+        while True:
+            if fast.next == NULL:
+                return slow
+            if fast.next.next == NULL:
+                return slow.next
+            fast = fast.next.next
+            slow = slow.next
